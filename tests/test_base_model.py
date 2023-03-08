76c1ee597d0336a2f3a7ace60361d17c0abdd2f9
olalekan <bhadmuslekan@gmail.com>
2023-03-07 23:51:47 -0800

    test base model

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""Define unittest classes"""
+import os
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Unittest cases instance"""
+
+    def test_unique_id(self):
+        self.assertNotEqual(BaseModel(self).id, BaseModel(12).id)
+
+if __name__ == '__main__':
+    unittest.main()
