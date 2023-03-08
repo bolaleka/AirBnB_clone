f9ba3c629af2bab4a2f36194d9c5bd67298b61ea
olalekan <bhadmuslekan@gmail.com>
2023-03-08 02:14:09 -0800

    test updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,7 +10,16 @@ class TestBaseModel(unittest.TestCase):
     """Unittest cases instance"""
 
     def test_unique_id(self):
+        """Test unique id"""
         self.assertNotEqual(BaseModel(self).id, BaseModel(12).id)
+
+    def test_save(self):
+        """Test if save method exist"""
+        try:
+            if type(BaseModel()).save.__name__ == "save":
+                print("OK")
+        except:
+            print("No file")
 
 if __name__ == '__main__':
     unittest.main()
