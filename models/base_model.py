4b46bb921b31282178ca3682ca82dcf5d424db1e
olalekan <bhadmuslekan@gmail.com>
2023-04-15 13:13:12 -0700

    Fix bug

--- models/base_model.py
+++ models/base_model.py
@@ -10,15 +10,11 @@ class BaseModel:
        attributes/methods for other classes
     """
 
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
-
     def __init__(self, *args, **kwargs):
         """ The init method """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.upadated_at = self.created_at
+        self.upadated_at = datetime.now()
 
         if kwargs is not None:
             f = "%Y-%m-%dT%H:%M:%S.%f"
@@ -39,20 +35,14 @@ class BaseModel:
 
     def save(self):
         """Upadte with public instance attribute"""
-        self.updated_at = BaseModel.updated_at
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values
             of __dict__ of the instance
         """
-        new_dict = {}
-        for key, values in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
-            else:
-                if not values:
-                    pass
-                else:
-                    new_dict[key] = values
+        new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
